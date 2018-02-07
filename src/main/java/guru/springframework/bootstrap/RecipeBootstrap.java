e2da161f3e48500498c8ca09a652314e4562e58e
Michael Kalisvaart <mkalisvaart@bol.com>
2018-02-07 13:39:39 +0100

    fixed import

--- src/main/java/guru/springframework/bootstrap/RecipeBootstrap.java
+++ src/main/java/guru/springframework/bootstrap/RecipeBootstrap.java
@@ -19,6 +19,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import static guru.springframework.domain.Difficulty.*;
+
 @Slf4j
 @Component
 public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEvent> {
