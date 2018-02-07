0bed744ebed7b3384e247b8a0b3ee8d8571155c6
Michael Kalisvaart <mkalisvaart@bol.com>
2018-02-07 16:11:31 +0100

    ingredientView and list

--- src/main/java/guru/springframework/converters/IngredientToIngredientCommand.java
+++ src/main/java/guru/springframework/converters/IngredientToIngredientCommand.java
@@ -12,13 +12,13 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class IngredientToIngredientCommand implements Converter<Ingredient, IngredientCommand> {
-
+    
     private final UnitOfMeasureToUnitOfMeasureCommand uomConverter;
-
+    
     public IngredientToIngredientCommand(UnitOfMeasureToUnitOfMeasureCommand uomConverter) {
         this.uomConverter = uomConverter;
     }
-
+    
     @Synchronized
     @Nullable
     @Override
@@ -26,9 +26,12 @@ public class IngredientToIngredientCommand implements Converter<Ingredient, Ingr
         if (ingredient == null) {
             return null;
         }
-
+        
         IngredientCommand ingredientCommand = new IngredientCommand();
         ingredientCommand.setId(ingredient.getId());
+        if (ingredient.getRecipe() != null) {
+            ingredientCommand.setRecipeId(ingredient.getRecipe().getId());
+        }
         ingredientCommand.setAmount(ingredient.getAmount());
         ingredientCommand.setDescription(ingredient.getDescription());
         ingredientCommand.setUom(uomConverter.convert(ingredient.getUom()));
--- src/test/java/guru/springframework/services/RecipeServiceIT.java
+++ src/test/java/guru/springframework/services/RecipeServiceIT.java
@@ -21,32 +21,33 @@ import static org.junit.Assert.assertEquals;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class RecipeServiceIT {
-    private static final String NEW_DESCRIPTION = "New Description";
-
+    
+    public static final String NEW_DESCRIPTION = "New Description";
+    
     @Autowired
     RecipeService recipeService;
-
+    
     @Autowired
     RecipeRepository recipeRepository;
-
+    
     @Autowired
     RecipeCommandToRecipe recipeCommandToRecipe;
-
+    
     @Autowired
     RecipeToRecipeCommand recipeToRecipeCommand;
-
+    
     @Transactional
     @Test
-    public void testSaveOfDescription() {
+    public void testSaveOfDescription() throws Exception {
         //given
         Iterable<Recipe> recipes = recipeRepository.findAll();
         Recipe testRecipe = recipes.iterator().next();
         RecipeCommand testRecipeCommand = recipeToRecipeCommand.convert(testRecipe);
-
+        
         //when
         testRecipeCommand.setDescription(NEW_DESCRIPTION);
         RecipeCommand savedRecipeCommand = recipeService.saveRecipeCommand(testRecipeCommand);
-
+        
         //then
         assertEquals(NEW_DESCRIPTION, savedRecipeCommand.getDescription());
         assertEquals(testRecipe.getId(), savedRecipeCommand.getId());
